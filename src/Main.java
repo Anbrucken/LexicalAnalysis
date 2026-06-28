dc159171e29eda813b4606cf5d551ddcd25de686
Anbrucken <Nick.hf19@gmail.com>
2026-06-28 20:56:56 +0200

    Added Tokenizer

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Tokenizer tokenizer = new Tokenizer();
+        try{
+            if(args.length == 0){
+                throw new Exception("no arguments");
+            }
+            else{
+                String fileName = args[0];
+                tokenizer.readInput(fileName);
+                tokenizer.print();
+            }
+
         }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
---
+++ src/Tokenizer.java
@@ -0,0 +1,188 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.lang.*;
+import java.util.Scanner;
+
+public class Tokenizer {
+    private ArrayList<Token> tokens = new ArrayList<>();
+
+    private static class Token {
+        private TokenType tokenType = TokenType.UNKNOWN;
+        private String charValue = null;
+        private long numValue = 0;
+        public Token(TokenType tokenType, String charValue){
+            this.tokenType = tokenType;
+            this.charValue = charValue;
+        }
+        public Token(TokenType tokenType, String charValue, long numValue){
+            this.tokenType = tokenType;
+            this.charValue = charValue;
+            this.numValue = numValue;
+        }
+        public Token(TokenType tokenType, long numValue){
+            this.tokenType = tokenType;
+            this.numValue = numValue;
+        }
+        public TokenType getTokenType() {
+            return tokenType;
+        }
+        public String getCharValue() {
+            return charValue;
+        }
+        public long getNumValue() {
+            return numValue;
+        }
+        public String toString() {
+            return "|" + tokenType.toString() + "| char: " + charValue + " | num: " + numValue + "|";
+        }
+
+    }
+
+    private enum TokenType{
+        IDENTIFIER,
+        NUMBER,
+        NOT,
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE,
+        AND,
+        OR,
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACKET,
+        RIGHT_BRACKET,
+        RETURN,
+        SEMICOLON,
+        COMMA,
+        COLON,
+        IF,
+        ELSE,
+        WHILE,
+        ASSIGN,
+        DATA_TYPE,
+        IGNORE,
+        UNKNOWN
+    }
+
+    public void readInput(String fileName){
+        try{
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            char c = '\0';
+            StringBuilder sb = new StringBuilder();
+            TokenType currentTokenType = TokenType.UNKNOWN;
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                for(int i = 0; i < line.length(); i++){
+                    if(c != '\0'){
+
+                        currentTokenType = determineTokenType(sb.toString());
+                        if(fitsTokenType(c, currentTokenType)){
+                            sb.append(c);
+                        }
+                        else{
+                            String tokenValue = sb.toString();
+                            if(currentTokenType == TokenType.NUMBER){
+
+                                this.tokens.add(new Token(currentTokenType, Long.parseLong(tokenValue)));
+                            }
+                            else{
+                                this.tokens.add(new Token(currentTokenType,tokenValue));
+                            }
+                            sb = new StringBuilder();
+                            sb.append(c);
+
+                        }
+
+                    }
+                    c = line.charAt(i);
+                }
+
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found: " + fileName);
+        }
+
+    }
+
+    private TokenType determineTokenType(String s){
+        if(s.isEmpty()) return TokenType.UNKNOWN;
+        switch(s){
+            case " ", "#":
+                return TokenType.IGNORE;
+            case "(":
+                return TokenType.LEFT_PAREN;
+            case ")":
+                return TokenType.RIGHT_PAREN;
+            case "[":
+                return TokenType.LEFT_BRACKET;
+            case "]":
+                return TokenType.RIGHT_BRACKET;
+            case "not":
+                return TokenType.NOT;
+            case "+":
+                return TokenType.PLUS;
+            case "-":
+                return TokenType.MINUS;
+            case "*":
+                return TokenType.MULTIPLY;
+            case "/":
+                return TokenType.DIVIDE;
+            case "and", "&&":
+                return TokenType.AND;
+            case "or", "||":
+                return TokenType.OR;
+            case "return":
+                return TokenType.RETURN;
+            case ";":
+                return TokenType.SEMICOLON;
+            case ":":
+                return TokenType.COLON;
+            case ",":
+                return TokenType.COMMA;
+            case "if":
+                return TokenType.IF;
+            case "else":
+                return TokenType.ELSE;
+            case "while":
+                return TokenType.WHILE;
+            case ":=":
+                return TokenType.ASSIGN;
+            case "int", "bool", "char", "string":
+                return TokenType.DATA_TYPE;
+            default:
+                if(Character.isDigit(s.charAt(s.length()-1))){
+                    return TokenType.NUMBER;
+                }
+                else if(Character.isLetter(s.charAt(s.length()-1))){
+                    return TokenType.IDENTIFIER;
+                }
+                else{
+                    return TokenType.UNKNOWN;
+                }
+
+        }
+    }
+
+    private boolean fitsTokenType(char c, TokenType currentTokenType){
+        return switch (currentTokenType) {
+            case IDENTIFIER -> Character.isLetter(c);
+            case NUMBER -> Character.isDigit(c);
+            default -> false;
+        };
+    }
+
+
+
+    public void print(){
+        for(Token token : tokens){
+            if(token.getTokenType() != TokenType.IGNORE && token.getTokenType() != TokenType.UNKNOWN){
+                System.out.println(token);
+            }
+
+        }
+    }
+}
